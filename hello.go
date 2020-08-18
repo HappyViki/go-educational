9a522f50c915964b4b194668554c769c8740b686
Veronica Eulenberg <veronica.eulenberg@gmail.com>
2020-08-18 15:18:59 -0700

    madlib

--- hello.go
+++ hello.go
@@ -7,5 +7,20 @@ import (
 )
 
 func main() {
-	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+	fmt.Println(morestrings.ReverseRunes("oG olleH"))
+	fmt.Println("Give me some words, and I'll tell you a story!")
+	
+	fmt.Print("Adjective: ")
+	var adj string
+	fmt.Scanln(&adj)
+
+	fmt.Print("Noun: ")
+	var noun string
+	fmt.Scanln(&noun)
+
+	fmt.Print("Verb: ")
+	var verb string
+	fmt.Scanln(&verb)
+
+	fmt.Printf("\nOnce upon a time, there was a %v %v. Every day, %v took a %v around the block. One day, %v stopped. The end.\n", adj, noun, noun, verb, noun)
 }
