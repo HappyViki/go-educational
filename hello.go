096715460fc15ac203e3b91619b9cda85defd9de
Veronica Eulenberg <veronica.eulenberg@gmail.com>
2020-08-18 13:16:30 -0700

    Importing packages from your module

--- hello.go
+++ hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/HappyViki/go-educational/morestrings"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
 }
